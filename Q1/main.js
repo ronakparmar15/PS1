c7ccd8887790dd1428ca5b0be06ae7e1ccf7b577
Ronak <ronakjparmar1553@gmail.com>
2023-08-03 20:09:35 +0530

    PS1

--- Q1/main.js
+++ Q1/main.js
@@ -11,7 +11,7 @@ const server=http.createServer((req,res)=>{
             if(err)
             {
                 res.writeHead(404,{'Content-Type': 'text/html'});
-                return res.end("404 ");
+                return res.end("404 page not found!");
             }
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(data);
